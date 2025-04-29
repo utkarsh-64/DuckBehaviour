a3f87ba5c864b108128002c5a0cd45baaff26a76
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 15:55:36 +0530

    Create abstract class Duck

--- Duck.java
+++ Duck.java
@@ -1,1 +1,31 @@
+//Duck.java
+abstract public class Duck {
+    QuackBehaviour quackBehaviour;
+    SwimBehaviour swimBehaviour;
+    FlyBehaviour flyBehaviour;
 
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+
+    public void performQuack(){
+        quackBehaviour.quack();
+    }
+
+    public void setSwimBehaviour(SwimBehaviour swimBehaviour) {
+        this.swimBehaviour = swimBehaviour;
+    }
+
+    public void performSwim(){
+        swimBehaviour.swim();
+    }
+
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
+
+    public void performFly(){
+        flyBehaviour.fly();
+    }
+    abstract public void display();
+}
