0925f605c1a4d5e53cd92990b37116e0aa9f38bb
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 16:00:49 +0530

    Create MallardDuck class extending abstract class Duck

--- MallardDuck.java
+++ MallardDuck.java
@@ -1,1 +1,13 @@
+//MallardDuck.java
+public class MallardDuck extends Duck {
+    public MallardDuck(){
+        quackBehaviour = new Quack();
+        swimBehaviour = new Swimming();
+        flyBehaviour = new Fly();
+    }
+    @Override
+    public void display() {
+        System.out.println("It's a Mallard Duck");
+    }
 
+}
