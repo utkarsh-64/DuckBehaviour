902a672bb5e5d90bf7b35f267b4ea49f6867323c
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 16:04:14 +0530

    Create RubberDuck class extending abstract class Duck

--- RubberDuck.java
+++ RubberDuck.java
@@ -1,1 +1,12 @@
-
+//RubberDuck.java
+public class RubberDuck extends Duck{
+    public RubberDuck(){
+        quackBehaviour = new Squeak();
+        swimBehaviour = new Floating();
+        flyBehaviour = new NoFly();
+    }
+    @Override
+    public void display() {
+        System.out.println("It's a Rubber Duck");
+    }
+}
