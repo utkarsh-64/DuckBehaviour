5ea1b2b41bebdf49fc8d99f9e39d0493c3e7e044
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 15:56:25 +0530

    Create DecoyDuck class extending abstract class Duck

--- DecoyDuck.java
+++ DecoyDuck.java
@@ -1,1 +1,12 @@
-
+//DecoyDuck.java
+public class DecoyDuck extends Duck {
+    public DecoyDuck() {
+        quackBehaviour = new MuteQuack();
+        swimBehaviour = new Floating();
+        flyBehaviour = new NoFly();
+    }
+    @Override
+    public void display() {
+        System.out.println("It's a Decoy Duck");
+    }
+}
