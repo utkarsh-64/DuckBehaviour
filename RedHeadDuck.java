ec24b0c30747d18bd0c733c95540e53b31fec2db
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 16:03:44 +0530

    Create RedHeadDuck class extending abstract class Duck

--- RedHeadDuck.java
+++ RedHeadDuck.java
@@ -1,1 +1,13 @@
+//RedHeadDuck.java
+public class RedHeadDuck extends Duck {
+    public RedHeadDuck(){
+        quackBehaviour = new Quack();
+        swimBehaviour =  new Swimming();
+        flyBehaviour = new Fly();
+    }
+    @Override
+    public void display() {
+        System.out.println("It's a Redhead Duck");
+    }
+}
 
